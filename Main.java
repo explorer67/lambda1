36a5e41074aa445dfa87a59915e4ebf92fe28391
Andrei Piskunov <explorer67@mail.ru>
2021-12-24 12:52:47 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+package homework_lambda1;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Calculator calc = Calculator.instance.get();
+
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1,1);
+            try {
+                int c = calc.devide.apply(a, b);
+                calc.println.accept(c);
+            } catch (ArithmeticException exception) {
+                System.out.println("Деление на ноль");
+            }
+
+            int d = -10;
+            int e = calc.abs.apply(d);
+            calc.println.accept(e);
+        }
+
+}
